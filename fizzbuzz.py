25bd741dc079862350778b4554b442c3d596d963
Eric Groves <grovesde@gmail.com>
2024-03-21 16:16:56 -0400

    my first

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
+for num in range (1,101):
+     
+    if num % 3 == 0 and num % 5 == 0:
+        print("FIZZBUZZ")
+    elif num % 3 == 0:
+        print("FIZZ")
+    elif num % 5 == 0:
+        print("BUZZ") 
+    else:       
+        print(num)
 
